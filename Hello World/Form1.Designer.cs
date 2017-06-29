3f8412d9418544ad8fe7085be30ecb4f4b2cb649
eaathan <andersen@mrthiis.dk>
2017-06-29 22:52:04 +0200

    sdasd

--- Hello World/Form1.Designer.cs
+++ Hello World/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(637, 411);
+            this.ClientSize = new System.Drawing.Size(671, 434);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
